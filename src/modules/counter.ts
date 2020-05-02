27443a8c0780d37e65b79c864a1b64a0525c7630
bopboy2 <scp0000@gmail.com>
2020-05-02 18:49:36 +0900

    08 redux with typescript"
    "

---
+++ src/modules/counter.ts
@@ -0,0 +1,21 @@
+const INCREASE = 'counter/INCREASE' as const;
+const DECREASE = 'counter/DECRASE' as const;
+const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const inceaseBy = (diff: number) => ({ type: INCREASE_BY, payload: diff });
+
+type CounterState = { count: number };
+const initialState: CounterState = { count: 0 };
+
+type CounterAction = ReturnType<typeof increase> | ReturnType<typeof decrease> | ReturnType<typeof inceaseBy>;
+function counter(state: CounterState = initialState, action: CounterAction): CounterState {
+    switch (action.type) {
+        case INCREASE: return { count: state.count + 1 };
+        case DECREASE: return { count: state.count - 1 };
+        case INCREASE_BY: return { count: state.count + action.payload };
+        default: return state;
+    }
+}
+export default counter;

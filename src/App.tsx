55afe2222e71ea547cbd9fde4f582431336643db
bopboy2 <scp0000@gmail.com>
2020-05-02 22:54:33 +0900

    10 refactoring with typesafe-actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import TodoApp from './containers/TodoApp';
+import CounterContainer from './containers/CounterContainer';
 
 function App() {
   return (
-    <TodoApp />
+    <>
+      <CounterContainer />
+      <hr />
+      <TodoApp />
+    </>
   );
 }
 
--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,21 +1,23 @@
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECRASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+import { createStandardAction, ActionType, createReducer } from 'typesafe-actions';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-export const inceaseBy = (diff: number) => ({ type: INCREASE_BY, payload: diff });
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECRASE';
+const INCREASE_BY = 'counter/INCREASE_BY';
+
+export const increase = createStandardAction(INCREASE)();
+export const decrease = createStandardAction(DECREASE)();
+export const inceaseBy = createStandardAction(INCREASE_BY)<number>();
 
 type CounterState = { count: number };
 const initialState: CounterState = { count: 0 };
 
-type CounterAction = ReturnType<typeof increase> | ReturnType<typeof decrease> | ReturnType<typeof inceaseBy>;
-function counter(state: CounterState = initialState, action: CounterAction): CounterState {
-    switch (action.type) {
-        case INCREASE: return { count: state.count + 1 };
-        case DECREASE: return { count: state.count - 1 };
-        case INCREASE_BY: return { count: state.count + action.payload };
-        default: return state;
-    }
-}
+const actions = { increase, decrease, inceaseBy };
+type CounterAction = ActionType<typeof actions>;
+
+const counter = createReducer<CounterState, CounterAction>(initialState, {
+    [INCREASE]: state => ({ count: state.count + 1 }),
+    [DECREASE]: state => ({ count: state.count - 1 }),
+    [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload })
+})
+
 export default counter;
